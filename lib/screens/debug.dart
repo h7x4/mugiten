d3d7fbc50520ff89b3be1ad877f771d681babf6f
h7x4 <h7x4@nani.wtf>
2025-06-01 15:21:42 +0200

    screens/debug: update with different component

--- lib/screens/debug.dart
+++ lib/screens/debug.dart
@@ -1,27 +1,47 @@
 import 'package:flutter/material.dart';
-
-import '../components/drawing_board/drawing_board.dart';
+import 'package:mugiten/database/database.dart';
+import 'package:mugiten/models/history/history_entry.dart';
 
 class DebugView extends StatelessWidget {
   const DebugView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        DrawingBoard(
-          allowHiragana: true,
-          allowKatakana: true,
-          allowOther: true,
-          onSuggestionChosen: (s) => ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Chose: $s'),
-              duration: const Duration(milliseconds: 600),
-            ),
+    return FutureBuilder(
+      future: GetIt.instance.get<Database>().rawQuery(
+        """
+        SELECT name, type
+        FROM sqlite_master
+        WHERE name NOT LIKE 'sqlite_%'
+        ORDER BY name
+        """,
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) return ErrorWidget(snapshot.error!);
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Debug View'),
           ),
-        ),
-      ],
+          body: ListView.builder(
+            itemCount: (snapshot.data as List<Map<String, dynamic>>).length,
+            itemBuilder: (context, index) {
+              final data = (snapshot.data as List<Map<String, dynamic>>)[index];
+              final tableName = (data['name'] as String) +
+                  (data['type'] == 'table' ? '' : ' (${data['type']})');
+              return ListTile(
+                title: Text(tableName),
+                onTap: () {
+                  // Handle table tap if needed
+                },
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
