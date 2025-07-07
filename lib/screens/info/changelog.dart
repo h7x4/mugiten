2531b2bbab05ab292ae7c626113a1bfff97f4344
h7x4 <h7x4@nani.wtf>
2025-07-07 21:09:39 +0200

    screens/changelog: use direct dependency on markdown package

--- lib/screens/info/changelog.dart
+++ lib/screens/info/changelog.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
-import 'package:markdown/markdown.dart' show markdownToHtml, ExtensionSet;
+import 'package:markdown/markdown.dart' show ExtensionSet;
 
 class ChangelogView extends StatelessWidget {
   const ChangelogView({super.key});
