48cfa7f1c62fde9388f52435fbd5f97ea3b99eb3
h7x4 <h7x4@nani.wtf>
2025-07-07 16:01:45 +0200

    history: show history entry count on top

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -29,7 +29,7 @@ class HistoryView extends StatelessWidget {
 
         return OpaqueBox(
           child: ListView.separated(
-            itemCount: data.length + 1,
+            itemCount: data.length + 2,
             itemBuilder: historyEntryWithData(data),
             separatorBuilder:
                 historyEntrySeparatorWithData(data.values.toList()),
@@ -46,8 +46,9 @@ class HistoryView extends StatelessWidget {
         final HistoryEntry search = data[index];
         final DateTime searchDate = search.lastTimestamp;
 
-        if (index == 0 ||
-            !dateIsEqual(data[index - 1].lastTimestamp, searchDate)) {
+        if (index != 1 &&
+            (index == 0 ||
+                !dateIsEqual(data[index - 2].lastTimestamp, searchDate))) {
           return TextDivider(text: formatDate(roundToDay(searchDate)));
         }
 
@@ -60,12 +61,28 @@ class HistoryView extends StatelessWidget {
 
   Widget Function(BuildContext, int) historyEntryWithData(
     Map<int, HistoryEntry> data,
-  ) =>
-      (context, index) => (index == 0)
-          ? const SizedBox.shrink()
-          : HistoryEntryTile(
-              entry: data.values.toList()[index - 1],
-              objectKey: data.keys.toList()[index - 1],
-              onDelete: () => build(context),
-            );
+  ) {
+    return (context, index) {
+      return switch (index) {
+        0 => const SizedBox.shrink(),
+        1 => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Center(
+              child: Text(
+                '${data.length} distinct searches made',
+                style: const TextStyle(
+                  fontSize: 10,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+        int i => HistoryEntryTile(
+            entry: data.values.toList()[i - 2],
+            objectKey: data.keys.toList()[i - 2],
+            onDelete: () => build(context),
+          ),
+      };
+    };
+  }
 }
