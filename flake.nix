16194511919b3d02709ca32b2aaed549a7529d04
h7x4 <h7x4@nani.wtf>
2025-12-19 11:46:56 +0900

    Update android environment

--- flake.nix
+++ flake.nix
@@ -17,17 +17,17 @@
           allowUnfree = true;
         };
       };
-      androidSdk = (pkgs.androidenv.composeAndroidPackages {
-        buildToolsVersions = [ "33.0.1" "34.0.0" ];
+      androidPkgs = (pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [ "35.0.0" "36.1.0" ];
         platformVersions = [ "35" "36" ];
         abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
         cmakeVersions = [ "3.22.1" ];
         includeNDK = true;
         ndkVersions = [ "27.0.12077973" ];
-      }).androidsdk;
-    in f system pkgs androidSdk);
+      });
+    in f system pkgs androidPkgs);
   in {
-    devShells = forAllSystems (_: pkgs: androidSdk: {
+    devShells = forAllSystems (_: pkgs: androidPkgs: {
       default = let
         flutter' = pkgs.flutter335;
         jdk' = pkgs.jdk21;
@@ -35,12 +35,17 @@
         packages = [
           flutter'
           pkgs.sqlite-interactive
-          androidSdk
+          androidPkgs.androidsdk
           jdk'
         ];
         env = {
-          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
+          ANDROID_SDK_ROOT = "${androidPkgs.androidsdk}/libexec/android-sdk";
+          GRADLE_OPTS = let
+            buildToolsVersion = (lib.head androidPkgs.build-tools).version;
+            buildToolsDir = "${androidPkgs.androidsdk}/libexec/android-sdk/build-tools/${buildToolsVersion}";
+          in lib.concatStringsSep " " [
+            "-Dorg.gradle.project.android.aapt2FromMavenOverride=${buildToolsDir}/aapt2"
+          ];
           FLUTTER_SDK = "${flutter'}";
           JAVA_HOME = "${jdk'}/lib/openjdk";
           LIBSQLITE_PATH = "${pkgs.sqlite.out}/lib/libsqlite3.so";
