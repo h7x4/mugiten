b76d26fd9075cefec04ff9f8b79473286c6b62b3
h7x4 <h7x4@nani.wtf>
2026-05-30 17:11:13 +0900

    screens/library_view: add icon for quicklist

--- lib/screens/library/library_view.dart
+++ lib/screens/library/library_view.dart
@@ -5,6 +5,7 @@ import 'package:get_it/get_it.dart';
 import 'package:mugiten/components/common/loading.dart';
 import 'package:mugiten/components/library/library_list_tile.dart';
 import 'package:mugiten/models/library_list.dart';
+import 'package:mugiten/settings.dart';
 import 'package:sqflite/sqlite_api.dart';
 
 class LibraryView extends StatefulWidget {
@@ -48,6 +49,9 @@ class _LibraryViewState extends State<LibraryView> {
                   (final e) => LibraryListTile(
                     key: ValueKey(e.name),
                     library: e,
+                    leading: quickAddLibraryList.value == e.name
+                        ? const Icon(Icons.bookmark, color: Colors.blue)
+                        : null,
                     onDelete: getEntriesFromDatabase,
                     onRename: (_, _) => getEntriesFromDatabase(),
                   ),
