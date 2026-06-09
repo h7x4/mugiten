cbde34bc4616e820e035bba8300590614cb989ca
h7x4 <h7x4@nani.wtf>
2026-06-09 19:13:02 +0900

    kanji_result/stroke_order_gif: provide kanjivg data from jadb

--- lib/components/kanji/kanji_result_body/stroke_order_gif.dart
+++ lib/components/kanji/kanji_result_body/stroke_order_gif.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:jadb/search.dart';
 import 'package:kanimaji/kanimaji.dart';
+import 'package:kanimaji/primitives/point.dart';
+import 'package:kanimaji/svg_parser.dart';
 import 'package:mugiten/theme.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class StrokeOrderGif extends StatelessWidget {
   final String kanji;
@@ -32,6 +37,26 @@ class StrokeOrderGif extends StatelessWidget {
         borderRadius: BorderRadius.circular(10.0),
         child: Kanimaji(
           kanji: kanji,
+          kanjiDataProvider: (final kanji_) async {
+            final result = await GetIt.instance
+                .get<Database>()
+                .jadbSearchKanjiVGGraph(kanji_);
+
+            // TODO: fix data provider function type to return nullable results
+            if (result == null) {
+              throw Exception('KanjiVG graph not found for $kanji_');
+            }
+
+            return KanjiVGItem(kanji_, [
+              for (final p in result.paths)
+                KanjiVGPath(
+                  strokeNumber: p.pathId,
+                  type: p.type ?? '',
+                  svgPath: parsePath(p.svgPath),
+                  strokeNumberLabelPosition: Point(p.labelX, p.labelY),
+                ),
+            ]);
+          },
           strokeColor: Theme.of(context).colorScheme.onSurface,
           strokeUnfilledColor: menuGreyLightColors.foregroundColor!.withAlpha(
             0x40,
