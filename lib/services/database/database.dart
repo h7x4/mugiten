28554cfefca0fef682f82ebece03504c0fff0c45
h7x4 <h7x4@nani.wtf>
2026-06-10 09:52:00 +0900

    services/database: log db version checking on startup

--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -9,7 +9,8 @@ import 'package:mugiten/models/verify_tables.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart' show databaseFactoryFfi, sqfliteFfiInit;
+import 'package:sqflite_common_ffi/sqflite_ffi.dart'
+    show databaseFactoryFfi, sqfliteFfiInit;
 import 'package:sqlite3/sqlite3.dart' show sqlite3;
 
 export 'package:mugiten/services/database/database_reset.dart'
@@ -50,9 +51,22 @@ Future<bool> databaseNeedsReset() async {
   final databaseVersion = await database.getVersion();
   await database.close();
 
+  final mugitenVersion = databaseVersion >> 16;
+  final jadbVersion = databaseVersion & 0xFFFF;
+
+  print(
+    'Current database version: (mugiten: $mugitenVersion, jadb: $jadbVersion)',
+  );
+  print(
+    'Expected database version: (mugiten: $mugitenSchemaVersion, jadb: $jadbSchemaVersion)',
+  );
+
   if (databaseVersion < schemaVersion) {
+    print('Database reset needed');
     return true;
   }
+
+  print('No database reset needed');
 
   return false;
 }
